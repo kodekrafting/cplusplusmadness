24dd8ce545db804901271dd2dbba7d146d826f34
kodekrafting <kodekrafting@yahoo.com>
2016-09-24 18:17:00 -0500

    Fixed typo and revised pointers poem

--- pointers.cpp
+++ pointers.cpp
@@ -1,4 +1,4 @@
-#kodekrafting 2016
+//kodekrafting 2016
 
 #include <stdio.h>
 
@@ -6,15 +6,9 @@
 
 int happiness = starting_point;
 
-void spend_time(int * direction) {
-    direction = &happiness;
-}
-
 int main()
 {
-    int * exploring_the_unknown = NULL;
-    spend_time(exploring_the_unknown);
-    if (&exploring_the_unknown) {
+    int * exploring_the_unknown = &happiness;
+    if (exploring_the_unknown)
         happiness++;
-    }
 }
